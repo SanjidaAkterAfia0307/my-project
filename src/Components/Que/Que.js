567a7eaf2cee3c017130f3848517a6521040aa91
Afia <sanjidaakterafia0307@gmail,com>
2022-09-29 19:53:48 +0600

    responsive

--- src/Components/Que/Que.js
+++ src/Components/Que/Que.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Que = () => {
     return (
-        <div className='m-24'>
+        <div className='md:m-24'>
             <h1 className='text-6xl font-bold text-slate-700 text-center my-10'>Q&A</h1>
-            <div npm >
-                <div className='shadow-md p-12'>
-                    <h4 className='text-slate-800 text-center m-4 font-bold text-4xl'>How does react work</h4>
+            <div  >
+                <div className='shadow-md md:p-12 p-4'>
+                    <h4 className='text-slate-800 text-center m-4 font-bold md:text-4xl text-xl'>How does react work</h4>
                     <p>React is an open-source JavaScript library developed by the team at Facebook. It is used to build rich graphical user interfaces (UIs).React reads the objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM.React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process.</p>
                 </div>
-                <div className='shadow-md p-12'>
-                    <h4 className='text-slate-800 text-center m-4 font-bold text-4xl'>What is the use of useeffect react hooks</h4>
+                <div className='shadow-md md:p-12 p-4'>
+                    <h4 className='text-slate-800 text-center m-4 font-bold md:text-4xl text-xl'>What is the use of useeffect react hooks</h4>
                     <p>The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers. useEffect accepts two arguments.The useEffect Hook is built in a way that we can return a function inside it and this return function is where the cleanup happens. The cleanup function prevents memory leaks and removes some unnecessary and unwanted behaviors.</p>
                 </div>
-                <div className='shadow-md p-12'>
-                    <h4 className='text-slate-800 text-center m-4 font-bold text-4xl'>Difference between State and Props</h4>
+                <div className='shadow-md md:p-12 p-4'>
+                    <h4 className='text-slate-800 text-center m-4 font-bold md:text-4xl text-xl'>Difference between State and Props</h4>
                     <p><strong> There are some difference between State and Props :</strong> <br /><br />
                         <ul>
                             <li>Props are read-only.One the other hand,State changes can be asynchronous.</li>
